e44ce480582f17d368ad7eb8bc8ed0814f4f4e8c
alphonsotran <alphonsotran@gmail.com>
2017-11-02 20:01:58 +0100

    Add code to application.rb to complete API key setup

--- config/application.rb
+++ config/application.rb
@@ -29,3 +29,13 @@ module NciClinicalRails
     config.generators.system_tests = nil
   end
 end
+
+config_files = ['secrets.yml']
+
+    config_files.each do |file_name|
+      file_path = File.join(Rails.root, 'config', file_name)
+      config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+      config_keys.each do |k,v|
+        ENV[k.upcase] ||= v
+      end
+    end
